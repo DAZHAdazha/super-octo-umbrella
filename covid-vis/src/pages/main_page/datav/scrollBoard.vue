793d52886e15e42f7e09f6b41b7190719557a35d
DAZHA <47051206+DAZHAdazha@users.noreply.github.com>
2020-06-30 13:57:53 +0800

    样式

--- covid-vis/src/pages/main_page/datav/scrollBoard.vue
+++ covid-vis/src/pages/main_page/datav/scrollBoard.vue
@@ -1,69 +1,69 @@
 <template>
-  <div id="scroll-board">
-    <dv-scroll-board :config="config" v-if="completed" />
-    <dv-loading v-if="loading">Loading...</dv-loading>
+  <div id='scroll-board'>
+    <dv-scroll-board :config='config' v-if='completed' />
+    <dv-loading v-if='loading'>Loading...</dv-loading>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
 export default {
-  name: "ScrollBoard",
-  data() {
+  name: 'ScrollBoard',
+  data () {
     return {
       config: {
-        header: ["时间", "内容", "来源"],
+        header: ['时间', '内容', '来源'],
         data: [],
         index: true,
         columnWidth: [50,100, 500, 150],
-        align: ["center"],
+        align: ['center'],
         rowNum: 7,
-        headerBGC: "#1981f6",
+        headerBGC: '#1981f6',
         headerHeight: 45,
-        oddRowBGC: "rgba(0, 44, 81, 0.8)",
-        evenRowBGC: "rgba(10, 29, 50, 0.8)"
+        oddRowBGC: 'rgba(0, 44, 81, 0.8)',
+        evenRowBGC: 'rgba(10, 29, 50, 0.8)'
       },
       loading: true,
       completed: false
-    };
+    }
   },
-  mounted() {
-    this.getNews();
+  mounted () {
+    this.getNews()
   },
   methods: {
-    getNews() {
+    getNews () {
       axios
-        .get("/news", {
+        .get('/news', {
           headers: {
-            authoration: "apicode",
-            apicode: "2e5cda5ad3e14374bf59db91c877c3d3"
+            authoration: 'apicode',
+            apicode: '2e5cda5ad3e14374bf59db91c877c3d3'
           }
         })
         .then(res => {
           // console.log(res);
-          var news = res.data.newslist[0].news;
-          console.log(news);
+          var news = res.data.newslist[0].news
+          // console.log(news);
           for (var i = 0; i < news.length; i++) {
-            var arr = new Array();
-            arr[0] = news[i].pubDateStr;
+            var arr = new Array()
+            arr[0] = news[i].pubDateStr
             arr[1] =
-              "<a href=" + news[i].sourceUrl + ">" + news[i].title + "</a>";
-            arr[2] = news[i].infoSource;
-            this.config.data.push(arr);
+              '<a href=' + news[i].sourceUrl + '>' + news[i].title + '</a>'
+            arr[2] = news[i].infoSource
+            this.config.data.push(arr)
           }
-          this.loading = false;
-          this.completed = true;
-          this.config = { ...this.config };
+          this.loading = false
+          this.completed = true
+          this.config = { ...this.config }
         })
         .catch(err => {
-          console.log("Newsfail");
-        });
+          console.log('Newsfail')
+        })
     }
   }
-};
+}
 </script>
 
-<style lang="less">
+<style lang='less'>
 #scroll-board {
   width: 50%;
   box-sizing: border-box;
